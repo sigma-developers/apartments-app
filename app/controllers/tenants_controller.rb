8579056bfcf98fbd4b116723e9571ca1245070a3
davidnene <davienesh4@gmail.com>
2022-10-05 20:49:48 +0300

    Add tenants controller and configure CRUD operations

---
+++ app/controllers/tenants_controller.rb
@@ -0,0 +1,54 @@
+class TenantsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    rescue_from ActiveRecord::RecordInvalid, with: :render_uprocessable_entity_response
+    skip_before_action :verify_authenticity_token
+    
+    
+    #GET /tenants
+    def index
+        tenants = Tenant.all
+        render json: tenants
+    end
+
+    #GET /tenants/:id
+    def show
+        tenant = Tenant.find(params[:id])
+        render json: tenant
+    end
+
+    #POST /tenants
+    def create
+        tenant = Tenant.create!(tenant_params)
+        render json: tenant, status: :created
+    end
+
+    #PATCH /tenants
+    def update
+        tenant = Tenant.update!(tenant_params)
+        render json: tenant
+    end
+
+
+    #DELETE /tenants
+    def destroy
+        tenant = Tenant.find(params[:id])
+        tenant.destroy
+
+        head :no
+    end
+
+    private
+
+    def tenant_params
+        params.permit(:name, :age)
+    end
+
+    def render_not_found_response
+        render json: {error: "Tenant not found"}, status: :not_found
+    end
+
+    def render_uprocessable_entity_response(invalid)
+        render json: {errors: invalid.record.errors.full_messages}, status: :unpocessable_entity
+    end
+
+end
